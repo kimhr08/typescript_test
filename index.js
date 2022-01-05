f602ddd67913a48f81470fa29d515a5e974c0dfd
MAC <mac@KIMELMAC.local>
2022-01-05 16:01:15 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var 이름 = "김혜리";
+var 나이 = 30;
+var 출생지역 = "부천";
+var like = {
+    song: "낭만고양이",
+    singer: "체리필터",
+};
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
---
+++ index.ts
@@ -0,0 +1,14 @@
+let 이름: string = "김혜리";
+let 나이: number = 30;
+let 출생지역: string = "부천";
+
+let like: { song: string; singer: string } = {
+  song: "낭만고양이",
+  singer: "체리필터",
+};
+
+let project: { member: string[]; days: number; started: boolean } = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
